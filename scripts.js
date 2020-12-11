2d09f89b22a698af50a7f57c2d216a3c75631a29
Scott Davies <scottdavies1983@gmail.com>
2020-12-11 11:37:05 +0000

    resolved cors issue with proxy

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const baseEndPoint = "http://www.recipepuppy.com/api";
+const proxy = "https://cors-anywhere.herokuapp.com/";
+
+async function fetchRecipes(query) {
+  const res = await fetch(`${proxy}${baseEndPoint}?q=${query}`);
+  const data = await res.json();
+  console.log(data);
+}
+
+fetchRecipes("pizza");
